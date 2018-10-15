2026c46491dc8f479f0ec51929f1f18b997e396f
Christian D Langevin <langevin@usgs.gov>
2018-10-15 11:48:36 -0500

    More work on exercises

--- exercises/Flopy/config.py
+++ exercises/Flopy/config.py
@@ -17,20 +17,19 @@ elif 'win' in sys.platform.lower():
 else:
     raise Exception('Could not find binaries for {}'.format(sys.platform))    
 
+def add_to_config(name, exeext):
+    exe = os.path.abspath(os.path.join(exepth, name.format(exeext)))
+    return exe
+
+
 mfexe = os.path.abspath(os.path.join(exepth, 'mf2005{}'.format(exeext)))
-mpexe = os.path.abspath(os.path.join(exepth, 'mp6{}'.format(exeext)))
+mpexe = os.path.abspath(os.path.join(exepth, 'mp7{}'.format(exeext)))
 mtexe = os.path.abspath(os.path.join(exepth, 'mt3dms{}'.format(exeext)))
-swexe = os.path.abspath(os.path.join(exepth, 'swt_v4{}'.format(exeext)))
+mf6exe = os.path.abspath(os.path.join(exepth, 'mf6{}'.format(exeext)))
 
-mf6exe =       os.path.abspath(os.path.join(exepth, 'mf6{}'.format(exeext)))
-mf2000exe =    os.path.abspath(os.path.join(exepth, 'mf2000{}'.format(exeext)))
-mflgrexe =     os.path.abspath(os.path.join(exepth, 'mflgr{}'.format(exeext)))
-mfnwtexe =     os.path.abspath(os.path.join(exepth, 'mfnwt{}'.format(exeext)))
-mfusgexe =     os.path.abspath(os.path.join(exepth, 'mfusg{}'.format(exeext)))
-mt3dusgsexe =  os.path.abspath(os.path.join(exepth, 'mt3dusgs{}'.format(exeext)))
-
-exelist = [mfexe, mpexe, mtexe, swexe, mf6exe, mf2000exe, mflgrexe,
-           mfnwtexe, mfusgexe, mt3dusgsexe]
+exelist = [mfexe, mpexe, mtexe, mf6exe]
 for e in exelist:
     if not os.path.isfile(e):
         print('Executable file could not be found: {}'.format(e))
+    else:
+        print('Executable file found: {}'.format(e))
